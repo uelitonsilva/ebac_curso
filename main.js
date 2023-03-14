dbc807858bfe870855f1e1bb214b40a4135ac42c
ueliton_silva <ueliton45@hotmail.com>
2023-03-14 16:25:36 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,15 @@
+$('form').on('submit', function(e){
+  e.preventDefault();
+   const inputTarefa = $('#nome-tarefa[type="text"]').val();
+    const novaTarefa = $('<li></li>')
+  
+  $(`<div><p>${inputTarefa}</p></div>`).appendTo(novaTarefa);
+
+  $(novaTarefa).appendTo('ul');
+
+  $('#nome-tarefa[type="text"]').val('');
+});
+
+$('ul').on('click', 'li', function() {
+  $(this).toggleClass('tarefa-concluida');
+});
